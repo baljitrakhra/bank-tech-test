fed9dddcae3578531d7fe03f2ea9e5b6c06691f5
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-05-20 19:07:35 +0100

    user  can now withdraw money from the bank account. Test in passing as well

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,18 @@
 class Account
-  attr_reader :balance, :deposit
+  attr_reader :balance, :deposit, :withdraw
 
   def initialize
     @balance = 0
     @deposit = []
+    @withdraw = []
   end
 
   def deposit_money(amount)
     @deposit << [amount, Time.new.strftime("%d/%m/%Y") ]
+  end
+
+  def withdraw_money(amount)
+    # will need to add a edge case to check balance before withdrwing 
+    @withdraw << [amount, Time.new.strftime("%d/%m/%Y") ]
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,5 +11,12 @@ describe Account do
       account.deposit_money(10)
       expect(account.deposit).to include [10, Time.new.strftime("%d/%m/%Y")]
     end
+
+    it " will let you withdraw money" do
+      account = Account.new
+      account.deposit_money(10)
+      account.withdraw_money(5)
+      expect(account.withdraw).to include [5, Time.new.strftime("%d/%m/%Y") ]
+    end
   end
 end
