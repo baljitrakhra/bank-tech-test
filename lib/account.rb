953e586d328c42530071f45eeac4401ca2fe7e02
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-05-21 00:25:19 +0100

    printing of the statment feature is complete and test is working

--- lib/account.rb
+++ lib/account.rb
@@ -2,21 +2,28 @@ class Account
   attr_reader :balance, :bank_statment
 
   def initialize
-    @balance = 0
+    @balance = 0.00
     @bank_statment = []
   end
 
   def deposit_money(amount)
     @balance += amount
     time = Time.new.strftime("%d/%m/%Y")
-    @bank_statment.push({ time: time, credit: amount, debit: "", balance: @balance })
+    @bank_statment.push({ "time": time, "credit": amount.to_f, "debit": "", "balance": @balance })
   end
 
   def withdraw_money(amount)
     # will need to add a edge case to check balance before withdrwing 
     @balance -= amount
     time = Time.new.strftime("%d/%m/%Y")
-    @bank_statment.push({ time: time, credit: "", debit: amount, balance: @balance })
+    @bank_statment.push({ "time": time, "credit": "", "debit": amount.to_f, "balance": @balance })
   end
-  
+
+  def print_statment
+    puts "date || credit || debit || balance"
+    @bank_statment.each do |activity|
+      print " #{activity[:time]} || #{activity[:credit]} || #{activity[:debit]} || #{activity[:balance]} \n"
+    end
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,19 +4,37 @@ describe Account do
   context "bank account" do
     it "is initialised with a zero balance" do
       account = Account.new
-      expect(account.balance).to equal 0
+      expect(account.balance).to equal 0.00
     end
     it "will accept money" do
       account = Account.new
       account.deposit_money(10)
-      expect(account.balance).to eq 10
+      expect(account.balance).to eq 10.00
     end
 
     it " will let you withdraw money" do
       account = Account.new
       account.deposit_money(10)
       account.withdraw_money(5)
-      expect(account.balance).to eq 5
+      expect(account.balance).to eq 5.00
+    end
+
+    it " will print a bank statment" do
+      account = Account.new
+      account.deposit_money(10)
+      account.withdraw_money(5)
+      expect { account.print_statment }.not_to raise_error
+    end
+
+    it " will print a bank statment for deposits" do
+      account = Account.new
+      account.deposit_money(10)
+      expect {account.print_statment }.to output( /\d{2} \d{2} \d{4} || 10 ||  || 10/).to_stdout
+    end
+    it " will print a bank statment for withdrawal" do
+      account = Account.new
+      account.withdraw_money(5)
+      expect { account.print_statment }.to output( /\d{2} \d{2} \d{4} || 5 ||  || -5/).to_stdout
     end
   end
 end
