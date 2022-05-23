5cefbde107f5f96eff0c50e11e8594a7ff896413
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-05-23 12:16:41 +0100

    transaction class created and it records the  time of the transactions

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,13 @@
 class Transaction
+  def initialize
+    @time = Time.new
+  end
+  
+  def display
+    "#{time} || || || "
+  end
 
+  def time
+    @time.strftime('%d/%m/%Y')
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,7 +3,7 @@ require 'transaction'
 describe Transaction do
   context '#time' do
     it ' shows time of the transaction' do
-      expect(subject.display).to eq " #{Time.new.strftime("%d/%m/%Y")} "
+      expect(subject.display).to eq "#{Time.new.strftime("%d/%m/%Y")} || || || "
     end
   end
 end
